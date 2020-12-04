05820c241afcbfebba450fcd0a7e054448a50066
Swapnil Ingle <dr.swapnilingle@gmail.com>
2020-12-04 08:42:18 +0530

    SI: Added recipe favorite/like functionality and other minor changes.
    1. Enhanced stylesheet.
    2. Added like as well as unlike functionality.

--- recipe-app/script.js
+++ recipe-app/script.js
@@ -1,12 +1,28 @@
 const meals = document.getElementById('meals');
+const favMealsList = document.getElementById('fav-meals');
+
 getRandomMeal();
+fetchFavMeals();
 
 async function getRandomMeal() {
-    const resp = await fetch("https://www.themealdb.com/api/json/v1/1/random.php");
-    const respData = await resp.json();
-    const randomMeal = respData.meals[0];
+    const randomMeal = await _extractMealDataFromAPI("https://www.themealdb.com/api/json/v1/1/random.php")
 
     _addMeal(randomMeal, true)
+    return randomMeal;
+}
+
+async function getMealById(id) {
+    return await _extractMealDataFromAPI("https://www.themealdb.com/api/json/v1/1/lookup.php?i=" + id);
+}
+
+async function getMealsBySearch(term) {
+    return await _extractMealDataFromAPI("https://www.themealdb.com/api/json/v1/1/search.php?s=" + term);
+}
+
+async function _extractMealDataFromAPI(apiEndpointURL) {
+    const resp = await fetch(apiEndpointURL);
+    const respData = await resp.json();
+    return respData.meals[0];
 }
 
 function _addMeal(mealData, random = false) {
@@ -20,7 +36,7 @@ function _addMeal(mealData, random = false) {
         </div>
         <div class="meal-body">
             <h4>${mealData.strMeal}</h4>
-            <button class="fav-btn"><i class="fas fa-heart"></i>
+            <button value="${mealData.idMeal}" onClick="toggleMealLike(this.value)" class="fav-btn" id="fav-btn"><i class="fas fa-heart"></i>
             </button>
         </div>
     `;
@@ -29,10 +45,64 @@ function _addMeal(mealData, random = false) {
     meals.appendChild(meal);
 }
 
-async function getMealById(id) {
-    const meal = await fetch("https://www.themealdb.com/api/json/v1/1/lookup.php?i=" + id);
+function toggleMealLike(mealId) {
+    const btn = document.getElementById("fav-btn");
+
+    if (btn.classList.contains("active")) {
+        _removeMealFromLocalStorage(mealId);
+        btn.classList.remove("active");
+    } else {
+        _addMealToLocalStorage(mealId);
+        btn.classList.add("active");
+    }
+
+    fetchFavMeals();
 }
 
-async function getMealsBySearch(term) {
-    const meals = await fetch("https://www.themealdb.com/api/json/v1/1/search.php?s=" + term);
+async function fetchFavMeals() {
+    // Clean container and refresh fav meals list
+    favMealsList.innerHTML = "";
+    const mealIds = _getMealsFromLocalStorage();
+
+    if (mealIds.length == 0) {
+        const noFavMealsAddedSpan = document.createElement('span');
+        noFavMealsAddedSpan.innerHTML = `Like a recipe to start tracking`;
+        favMealsList.appendChild(noFavMealsAddedSpan);
+    }
+
+    for (let i=0; i<mealIds.length; i++) {
+        const mealId = mealIds[i];
+        meal = await getMealById(mealId);
+        addMealToFav(meal);
+    }
+}
+
+function addMealToFav(mealData) {
+    const favMeal = document.createElement("div");
+
+    favMeal.innerHTML = `
+        <button value=${mealData.idMeal} onClick="toggleMealLike(this.value)" class="clearFavMeal">
+            <i class="fas fa-times-circle"></i>
+        </button>
+        <img src="${mealData.strMealThumb}" alt="${mealData.strMeal}"><br>
+        <span>${mealData.strMeal}</span>
+    `;
+
+    favMealsList.appendChild(favMeal);
+}
+
+function _addMealToLocalStorage(mealId) {
+    const mealIds = _getMealsFromLocalStorage();
+    localStorage.setItem('mealIds', JSON.stringify([...mealIds, mealId]));
+}
+
+function _getMealsFromLocalStorage() {
+    const mealIds = JSON.parse(localStorage.getItem('mealIds'));
+    return mealIds === null ? [] : mealIds;
+}
+
+function _removeMealFromLocalStorage(mealId) {
+    const mealIds = _getMealsFromLocalStorage();
+
+    localStorage.setItem('mealIds', JSON.stringify(mealIds.filter(id => id !== mealId)));
 }
