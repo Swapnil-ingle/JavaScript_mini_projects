df5703e665d0aa934494aef9e49f1009148dac07
Swapnil Ingle <dr.swapnilingle@gmail.com>
2020-12-17 08:45:22 +0530

    SI: Updated API link to HTTPS

--- movie-app/script.js
+++ movie-app/script.js
@@ -1,4 +1,4 @@
-const API_URL = 'http://api.themoviedb.org/3/discover/movie?sort_by=popularity.desc&api_key=04c35731a5ee918f014970082a0088b1&page=1';
+const API_URL = 'https://api.themoviedb.org/3/discover/movie?sort_by=popularity.desc&api_key=04c35731a5ee918f014970082a0088b1&page=1';
 const IMG_PATH = 'https://image.tmdb.org/t/p/w1280';
 
 const moviesContainerEl = document.getElementById('movies-container');
