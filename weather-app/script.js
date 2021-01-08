121748125fffe80e0163a2dcb9c9b16fe1450548
Swapnil Ingle <dr.swapnilingle@gmail.com>
2021-01-08 19:30:16 +0530

    SI: Fixed the API to fetch the weather by location

--- weather-app/script.js
+++ weather-app/script.js
@@ -1,9 +1,11 @@
-const APIURL = 'https://www.metaweather.com/api';
+const apiKey = "3265874a2c77ae4a04bb96236a642d2f";
+const url = (location) => `https://api.openweathermap.org/data/2.5/weather?q=${location}&appid=${apiKey}`;
 
-getWeatherByLocation('London');
+getWeatherByLocation("London");
 
 async function getWeatherByLocation(location) {
-    const resp = await fetch(APIURL + '/location/search?query=' + location);
-    const respData = await resp.json;
+    const resp = await fetch(url(location), { origin: "cors" });
+    const respData = await resp.json();
+
     console.log(respData);
 }
